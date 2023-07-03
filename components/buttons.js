981ca1b794ae783007c13b05324deff667109c5f
fadjariftikhar <fadjar.iftikhar@gmail.com>
2023-07-03 11:03:25 +0700

    jhee

---
+++ components/buttons.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export function NavBtn(label, url) {
+  return (
+    <Link
+      className=" text-slate-500 text-xl px-2 py-1 rounded hover:text-green-700 focus-within:text-green-700 outline-none font-black"
+      href={url}
+    >
+      {label}
+    </Link>
+  );
+}
