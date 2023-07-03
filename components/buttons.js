d6c96e527ef106a019c0b5a08072d50085e59dac
iftikharfadjar <fadjar.iftikhar@gmail.com>
2023-07-03 13:58:23 +0000

    fix issue props

--- components/buttons.js
+++ components/buttons.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-export function NavBtn(label, url) {
+export function NavBtn({label, url}) {
   return (
     <Link
       className=" text-slate-500 text-xl px-2 py-1 rounded hover:text-green-700 focus-within:text-green-700 outline-none font-black"
