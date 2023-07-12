29326e95889bc81da65d620da1a6520c7ad21ca7
iftikharfadjar <42471332+iftikharfadjar@users.noreply.github.com>
2023-07-12 04:50:08 +0000

    add footer

--- app/page.js
+++ app/page.js
@@ -9,41 +9,54 @@ export default function Home() {
           <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none"></div>
         </div> */}
 
-        <div className="h-[50vh] w-full relative mb-12">
-          <Image
-            // className="rounded-xl"
-            src="/asset/insert-photo-here.jpg"
-            alt="photo halaman depan"
-            fill // required
-          />
-        </div>
+       <MainPhoto  />
 
-        <div className="flex flex-row justify-around">
-          <div className="place-content-center">
-            <CardMd />
-          </div>
-
-          <div className="flex flex-row gap-10">
-            <div className="flex flex-col gap-10">
-              <div>
-                <CardMd />
-              </div>
-              <div>
-                <CardMd />
-              </div>
-            </div>
-            <div className="flex flex-col gap-10">
-              <div className="h-8"></div>
-              <div>
-                <CardMd />
-              </div>
-              <div>
-                <CardMd />
-              </div>
-            </div>
-          </div>
-        </div>
+        <CardHome />
+          
       </main>
     </>
   );
+}
+
+
+const MainPhoto = () => {
+  return (
+    <div className="h-[50vh] w-full relative mb-12">
+    <Image
+      // className="rounded-xl"
+      src="/asset/insert-photo-here.jpg"
+      alt="photo halaman depan"
+      fill // required
+    />
+  </div>
+  )
+}
+
+const CardHome = () => {
+  return (
+  <div className="flex h-screen w-full bg-slate-50 flex-row justify-around">
+    <div className="w-1/2 flex justify-center items-center">
+      <CardMd />
+    </div>
+
+    <div className="flex grow bg-slate-200 justify-around outline">
+      <div className="flex flex-col justify-around">
+        <div>
+          <CardMd />
+        </div>
+        <div>
+          <CardMd />
+        </div>
+      </div>
+      <div className="flex flex-col justify-around">
+        <div>
+          <CardMd />
+        </div>
+        <div>
+          <CardMd />
+        </div>
+      </div>
+    </div>
+  </div>
+  )
 }
